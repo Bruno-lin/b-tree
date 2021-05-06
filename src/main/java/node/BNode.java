13efcc7d7f931bc30f081d15afc1e21d3d261010
sen <yjghdsss@163.com>
2021-05-06 21:59:17 +0800

    更改数据结构

--- src/main/java/node/BNode.java
+++ src/main/java/node/BNode.java
@@ -1,18 +1,18 @@
 package node;
 
 import java.util.Arrays;
+import java.util.List;
 
-public class BNode{
+
+public class BNode {
 
     int degree = 0;         // 树的最小度数
 
-    int[] keys;             // 关键字集合
+    List<Integer> keys;     // 关键字集合
 
-    private int keyCount;   // key的数量
+    List<String> data;          // 数据,与关键字通过索引一一对应
 
-    String[] data;          // 数据,与关键字通过索引一一对应
-
-    BNode[] children;        // 子节点集合
+    List<BNode> children;       // 子节点集合
 
     BNode(int degree) {
 
@@ -20,8 +20,9 @@ public class BNode{
 
     /**
      * 分割子节点回新的子树
+     *
      * @param child 需要分割的满子节点
-     * @return  生成的子树
+     * @return 生成的子树
      */
     BNode splitChild(BNode child) {
         //TODO
@@ -31,18 +32,21 @@ public class BNode{
 
     /**
      * 插入数据
+     *
      * @param newKey 新插入的键值
-     * @param data  数据
+     * @param data   数据
+     * @return 新的根节点
      */
-    void insert(int newKey, String data) {
+    BNode insert(int newKey, String data) {
         //TODO
 
+        return null;
     }
 
     /**
      * 以键搜索对应的数据
      * @param key 特定的键
-     * @return 键对应的数据,键不存在则返回 null
+     * @return 键对应的数据, 键不存在则返回 null
      */
     String research(int key) {
         //TODO
@@ -52,14 +56,18 @@ public class BNode{
 
     /**
      * 在树中删除某个键和对应的数据
+     * @return 新的根节点
      */
-    void delete(int key) {
+    BNode delete(int key) {
+        //TODO
 
+        return null;
     }
 
     @Override
     public String toString() {
         //TODO
+
         return null;
     }
 }
